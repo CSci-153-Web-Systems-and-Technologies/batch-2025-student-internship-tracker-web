d3ba95048e6e25bc32f163cb31f86924306b2738
Jamesnel Chavez <officialketchupped@gmail.com>
2025-12-12 20:58:40 +0800

    fix:prerender

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,6 +1,28 @@
-export const dynamic = "force-dynamic";
 
-import LayoutClient from "./layout-client";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import NavBar from "@/components/navbar-user";
+import { Spinner } from "@/components/ui/shadcn-io/spinner";
+import { Suspense } from "react";
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <LayoutClient> {children} </LayoutClient>
+  
+  return (
+    <SidebarProvider >
+      <Suspense>
+        <AppSidebar />
+      </Suspense>
+        <SidebarInset className="flex flex-col min-h-screen bg-gradient-to-br from-slate-950 via-blue-950 to-slate-900 text-white text-white">
+          <NavBar />
+           <div className="pl-40 ">
+              <Suspense fallback={
+                <div className="flex items-center justify-center w-full py-20 pt-100">
+                  <Spinner variant="circle-filled" size={32} />
+                </div>
+              }>
+                {children}
+              </Suspense>
+        </div>
+        </SidebarInset>
+    </SidebarProvider>
+  );
 }
