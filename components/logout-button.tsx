16a7bb96e6378a3350057e734c1038669aabc587
Jamesnel Chavez <officialketchupped@gmail.com>
2025-12-14 15:25:06 +0800

    fix: Implement Fixes
    - Resolve Not being able to submit Files when you're in multiple Organization
    - Resolve Logout not Properly logging you out but redirecting you to login instead
    - Resolve users not having any names in organization members table.

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -3,14 +3,16 @@
 import { createClient } from "@/lib/supabase/client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-
 export function LogoutButton() {
   const router = useRouter();
 
   const logout = async () => {
     const supabase = createClient();
+
     await supabase.auth.signOut();
-    router.push("/login");
+
+    router.replace("/login");
+    router.refresh();
   };
 
   return <Button onClick={logout}>Logout</Button>;
